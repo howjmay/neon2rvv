8836ced5a813ec175778c03fa444594993b6fc86
Yang Hau <yuanyanghau@gmail.com>
2023-11-17 02:08:11 +0100

    feat: Add vqsub_s8

--- neon2rvv.h
+++ neon2rvv.h
@@ -684,7 +684,7 @@ FORCE_INLINE int16x8_t vmlsl_s8(int16x8_t __a, int8x8_t __b, int8x8_t __c) {
 
 // FORCE_INLINE uint32x4_t vhsubq_u32(uint32x4_t __a, uint32x4_t __b);
 
-// FORCE_INLINE int8x8_t vqsub_s8(int8x8_t __a, int8x8_t __b);
+FORCE_INLINE int8x8_t vqsub_s8(int8x8_t __a, int8x8_t __b) { return __riscv_vssub_vv_i8m1(__a, __b, 8); }
 
 // FORCE_INLINE int16x4_t vqsub_s16(int16x4_t __a, int16x4_t __b);
 
