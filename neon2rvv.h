92b73477f9c2734964855e8e190114a3ad4281c4
Yang Hau <yuanyanghau@gmail.com>
2024-04-11 18:35:31 +0200

    feat: Add v[max|min]nm[q]_f64

--- neon2rvv.h
+++ neon2rvv.h
@@ -3581,17 +3581,17 @@ FORCE_INLINE float32x2_t vmaxnm_f32(float32x2_t a, float32x2_t b) { return __ris
 
 FORCE_INLINE float32x4_t vmaxnmq_f32(float32x4_t a, float32x4_t b) { return __riscv_vfmax_vv_f32m1(a, b, 4); }
 
-// FORCE_INLINE float64x1_t vmaxnm_f64(float64x1_t a, float64x1_t b);
+FORCE_INLINE float64x1_t vmaxnm_f64(float64x1_t a, float64x1_t b) { return __riscv_vfmax_vv_f64m1(a, b, 1); }
 
-// FORCE_INLINE float64x2_t vmaxnmq_f64(float64x2_t a, float64x2_t b);
+FORCE_INLINE float64x2_t vmaxnmq_f64(float64x2_t a, float64x2_t b) { return __riscv_vfmax_vv_f64m1(a, b, 2); }
 
 FORCE_INLINE float32x2_t vminnm_f32(float32x2_t a, float32x2_t b) { return __riscv_vfmin_vv_f32m1(a, b, 2); }
 
 FORCE_INLINE float32x4_t vminnmq_f32(float32x4_t a, float32x4_t b) { return __riscv_vfmin_vv_f32m1(a, b, 4); }
 
-// FORCE_INLINE float64x1_t vminnm_f64(float64x1_t a, float64x1_t b);
+FORCE_INLINE float64x1_t vminnm_f64(float64x1_t a, float64x1_t b) { return __riscv_vfmin_vv_f64m1(a, b, 1); }
 
-// FORCE_INLINE float64x2_t vminnmq_f64(float64x2_t a, float64x2_t b);
+FORCE_INLINE float64x2_t vminnmq_f64(float64x2_t a, float64x2_t b) { return __riscv_vfmin_vv_f64m1(a, b, 2); }
 
 FORCE_INLINE uint8x16_t vmaxq_u8(uint8x16_t a, uint8x16_t b) { return __riscv_vmaxu_vv_u8m1(a, b, 16); }
 
