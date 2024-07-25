38cc54b17c026963dbf5c66944bd3114d8a00478
Yang Hau <yuanyanghau@gmail.com>
2024-07-26 00:58:21 +0800

    feat: Add vmlal_high_laneq_[s16|s32|u16|u32]

--- tests/impl.h
+++ tests/impl.h
@@ -1942,10 +1942,10 @@
   _(vmlal_laneq_s32)                                                             \
   _(vmlal_laneq_u16)                                                             \
   _(vmlal_laneq_u32)                                                             \
-  /*_(vmlal_high_laneq_s16)                                                   */ \
-  /*_(vmlal_high_laneq_s32)                                                   */ \
-  /*_(vmlal_high_laneq_u16)                                                   */ \
-  /*_(vmlal_high_laneq_u32)                                                   */ \
+  _(vmlal_high_laneq_s16)                                                        \
+  _(vmlal_high_laneq_s32)                                                        \
+  _(vmlal_high_laneq_u16)                                                        \
+  _(vmlal_high_laneq_u32)                                                        \
   _(vqdmlal_lane_s16)                                                            \
   _(vqdmlal_lane_s32)                                                            \
   /*_(vqdmlalh_lane_s16)                                                      */ \
