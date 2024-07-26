10997fcc4e7e5b9d6da38a00dff2e8b1ba3568eb
Yang Hau <yuanyanghau@gmail.com>
2024-07-26 11:37:08 +0800

    feat: Add vqdmlal_laneq_[s16|s32]

--- tests/impl.cpp
+++ tests/impl.cpp
@@ -31946,9 +31946,59 @@ result_t test_vqdmlal_high_lane_s32(const NEON2RVV_TEST_IMPL &impl, uint32_t ite
 #endif  // ENABLE_TEST_ALL
 }
 
-result_t test_vqdmlal_laneq_s16(const NEON2RVV_TEST_IMPL &impl, uint32_t iter) { return TEST_UNIMPL; }
+result_t test_vqdmlal_laneq_s16(const NEON2RVV_TEST_IMPL &impl, uint32_t iter) {
+#ifdef ENABLE_TEST_ALL
+  const int32_t *_a = (int32_t *)impl.test_cases_int_pointer1;
+  const int16_t *_b = (int16_t *)impl.test_cases_int_pointer2;
+  const int16_t *_c = (int16_t *)impl.test_cases_int_pointer3;
+  int32x4_t d;
+  int32_t _d[4];
+  int32x4_t a = vld1q_s32(_a);
+  int16x4_t b = vld1_s16(_b);
+  int16x8_t c = vld1q_s16(_c);
 
-result_t test_vqdmlal_laneq_s32(const NEON2RVV_TEST_IMPL &impl, uint32_t iter) { return TEST_UNIMPL; }
+#define TEST_IMPL(IDX)                                 \
+  for (int i = 0; i < 4; i++) {                        \
+    _d[i] = sat_add(_a[i], sat_dmull(_b[i], _c[IDX])); \
+  }                                                    \
+  d = vqdmlal_laneq_s16(a, b, c, IDX);                 \
+  CHECK_RESULT(validate_int32(d, _d[0], _d[1], _d[2], _d[3]))
+
+  IMM_8_ITER
+#undef TEST_IMPL
+
+  return TEST_SUCCESS;
+#else
+  return TEST_UNIMPL;
+#endif  // ENABLE_TEST_ALL
+}
+
+result_t test_vqdmlal_laneq_s32(const NEON2RVV_TEST_IMPL &impl, uint32_t iter) {
+#ifdef ENABLE_TEST_ALL
+  const int64_t *_a = (int64_t *)impl.test_cases_int_pointer1;
+  const int32_t *_b = (int32_t *)impl.test_cases_int_pointer2;
+  const int32_t *_c = (int32_t *)impl.test_cases_int_pointer3;
+  int64x2_t d;
+  int64x2_t a = vld1q_s64(_a);
+  int32x2_t b = vld1_s32(_b);
+  int32x4_t c = vld1q_s32(_c);
+  int64_t _d[2];
+
+#define TEST_IMPL(IDX)                                 \
+  for (int i = 0; i < 2; i++) {                        \
+    _d[i] = sat_add(_a[i], sat_dmull(_b[i], _c[IDX])); \
+  }                                                    \
+  d = vqdmlal_laneq_s32(a, b, c, IDX);                 \
+  CHECK_RESULT(validate_int64(d, _d[0], _d[1]))
+
+  IMM_4_ITER
+#undef TEST_IMPL
+
+  return TEST_SUCCESS;
+#else
+  return TEST_UNIMPL;
+#endif  // ENABLE_TEST_ALL
+}
 
 result_t test_vqdmlalh_laneq_s16(const NEON2RVV_TEST_IMPL &impl, uint32_t iter) { return TEST_UNIMPL; }
 
